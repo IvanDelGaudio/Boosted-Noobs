f878a74bd1cfc0532a23d7548d147cee82158560
Francesco Del Vecchio <francescodvecchio13@gmail.com>
2025-01-20 16:22:45 +0100

    Update Player Movement

---
+++ Assets/Scripts/Player/FirstPersonCamera.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerControl
+{
+    public class FirstPersonCamera : MonoBehaviour
+    {
+        [Header("Focus")]
+        public Transform target = null;
+
+        
+        public Vector2 mouseSensitivity = Vector2.one;
+        public Vector2 padSensitivity = Vector2.one * 75.0f;
+        public Vector2 padDrag = Vector2.one * 150.0f;
+        private Camera cam = null;
+
+        private float maxUpAngle = 50.0f;
+        private float maxDownAngle = -50.0f;
+        private float angleAroundVertical = 0.0f;
+        private float currentVelocityAroundVertical = 0.0f;
+        private float angleAroundRight = 0.0f;
+        private float currentVelocityAroundRight = 0.0f;
+
+        private Vector3 moveVelocity = Vector3.zero;
+        private Vector3 rotateVelocity = Vector3.zero;
+        void Awake()
+        {
+            cam = GetComponent<Camera>();
+        }
+        void LateUpdate()
+        {
+
+            currentVelocityAroundVertical = Mathf.MoveTowards(currentVelocityAroundVertical, 0, Time.deltaTime * padDrag.x);
+            currentVelocityAroundRight = Mathf.MoveTowards(currentVelocityAroundRight, 0, Time.deltaTime * padDrag.y);
+
+            AddRotationAroundVertical(Input.GetAxis("Look Horizontal") * mouseSensitivity.x);
+            AddRotationAroundRight(Input.GetAxis("Look Vertical") * mouseSensitivity.y);
+
+            Vector3 lookDirection = Vector3.forward;
+            lookDirection = Quaternion.AngleAxis(-angleAroundRight, Vector3.right) * lookDirection;
+            lookDirection = Quaternion.AngleAxis(angleAroundVertical, Vector3.up) * lookDirection;
+            transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+
+        }
+
+        private void AddRotationAroundVertical(float degrees)
+        {
+            angleAroundVertical += degrees;
+            angleAroundVertical = (angleAroundVertical + 360) % 360;
+        }
+        private void AddRotationRateAroundVertical(float degreesPerSecond)
+        {
+
+            AddRotationAroundVertical(currentVelocityAroundVertical * Time.deltaTime);
+            if (Mathf.Abs(degreesPerSecond) < 0.1f)
+                return;
+            if (Mathf.Sign(degreesPerSecond) != Mathf.Sign(currentVelocityAroundVertical))
+                currentVelocityAroundVertical = degreesPerSecond;
+            else
+                currentVelocityAroundVertical = Mathf.Sign(degreesPerSecond) * Mathf.Max(Mathf.Abs(currentVelocityAroundVertical), Mathf.Abs(degreesPerSecond));
+        }
+
+        private void AddRotationAroundRight(float degrees)
+        {
+            angleAroundRight += degrees;
+            angleAroundRight = Mathf.Clamp(angleAroundRight, maxDownAngle, maxUpAngle);
+        }
+        private void AddRotationRateAroundRight(float degreesPerSecond)
+        {
+
+            AddRotationAroundRight(currentVelocityAroundRight * Time.deltaTime);
+            if (Mathf.Abs(degreesPerSecond) < 0.1f)
+                return;
+            if (Mathf.Sign(degreesPerSecond) != Mathf.Sign(currentVelocityAroundRight))
+                currentVelocityAroundRight = degreesPerSecond;
+            else
+                currentVelocityAroundRight = Mathf.Sign(degreesPerSecond) * Mathf.Max(Mathf.Abs(currentVelocityAroundRight), Mathf.Abs(degreesPerSecond));
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerControllerMover.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PlayerControl
+{
+    public class PlayerControllerMover : MonoBehaviour
+    {
+        public enum OrientMode : byte
+        {
+            None, Movement, LookDirection
+        }
+        private CharacterController characterController = null;
+
+        [Header("Anchors")]
+        [SerializeField]
+        private Transform renderRoot;
+        [Header("Move params")]
+        [SerializeField]
+        [Min(0.25f)]
+        private float speed = 5.0f;
+        [SerializeField]
+        private OrientMode orientMode = OrientMode.Movement;
+        private float orientToReachTime = 0.5f;
+        private Vector3 orientToCurrentSpeed = Vector3.zero;
+        private float verticalSpeed = 0.0f;
+        private bool isGrounded
+        {
+            get
+            {
+               return characterController.isGrounded;
+            }
+        
+        }
+        void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+        void Start()
+        {
+            if (renderRoot != null)
+                renderRoot.position -= transform.up * characterController.skinWidth;
+        }
+        void FixedUpdate()
+        {
+            if (isGrounded)
+                verticalSpeed = 0.0f;
+            verticalSpeed -= 9.81f * Time.deltaTime;
+        }
+        void Update()
+        {
+
+            Vector3 moveDirection = Vector3.zero;
+            Transform cam = Camera.main.transform;
+            moveDirection += Input.GetAxis("Horizontal") * cam.right;
+            moveDirection += Input.GetAxis("Vertical") * cam.forward;
+            float moveMagnitude = moveDirection.magnitude;
+            moveDirection.y = 0.0f;
+            moveDirection.Normalize();
+            moveDirection *= moveMagnitude;
+
+            Move(moveDirection * speed);
+            if (orientMode != OrientMode.None)
+            {
+                Quaternion targetRotation = transform.rotation;
+                switch (orientMode)
+                {
+                    case OrientMode.Movement:
+                        if (moveDirection.sqrMagnitude > Mathf.Epsilon)
+                        {
+                            targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+                        }
+                        break;
+                    case OrientMode.LookDirection:
+                        Vector3 cameraLookDirection = Camera.main.transform.forward;
+                        cameraLookDirection.y = 0.0f;
+
+                        if (cameraLookDirection.sqrMagnitude > Mathf.Epsilon)
+                            targetRotation = Quaternion.LookRotation(cameraLookDirection, Vector3.up);
+                        break;
+                    default:
+                        break;
+                }
+                transform.rotation = SmoothDampRotation(transform.rotation, targetRotation, ref orientToCurrentSpeed, orientToReachTime);
+            }
+
+        }
+
+        public void Move(Vector3 direction)
+        {
+                direction += Vector3.up * verticalSpeed;
+                characterController.Move(direction * Time.deltaTime);
+            
+        }
+        private static Quaternion SmoothDampRotation(Quaternion from, Quaternion to, ref Vector3 currentVelocity, float smoothTime)
+        {
+            Vector3 fromEuler = from.eulerAngles;
+            Vector3 toEuler = to.eulerAngles;
+
+            Vector3 smoothedAngle = new Vector3(
+                Mathf.SmoothDampAngle(fromEuler.x, toEuler.x, ref currentVelocity.x, smoothTime),
+                Mathf.SmoothDampAngle(fromEuler.y, toEuler.y, ref currentVelocity.y, smoothTime),
+                Mathf.SmoothDampAngle(fromEuler.z, toEuler.z, ref currentVelocity.z, smoothTime)
+            );
+
+            return Quaternion.Euler(smoothedAngle);
+        }
+    }
+}
