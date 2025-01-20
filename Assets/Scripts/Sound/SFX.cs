82bef15e0975da5503f48d493762aa9afb88557c
IvanDelGaudio <ivandelgaudio0@gmail.com>
2025-01-20 19:51:10 +0100

    change sfx

--- Assets/Scripts/Sound/SFX.cs
+++ Assets/Scripts/Sound/SFX.cs
@@ -3,63 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-namespace Sound
+namespace Sound{
+public class SFX : SoundControls
     {
-        public class SFX : SoundControls
+    #region Public 
+        enum World : byte
         {
-            #region Public Variables
-            [Header("SFX")]
-            private AudioSource audioSource;
-            public AudioMixerGroup mixerGroup;
-            public AudioClip clip;
-            #endregion
-            #region Private Variables
-            SoundControls sourceControls;
-            #endregion
-            #region Lifecycle
-            private void Awake()
-            {
-                sourceControls = GameObject.Find("SoundManager").GetComponent<SoundControls>();
-            }
-            private void Start()
-            {
-                PlayBGM();
-            }
-            private void Update()
-            {
-                if (Input.GetKeyDown(KeyCode.A))
-                    StopBGM();
-                if (Input.GetKeyDown(KeyCode.B))
-                    ResumeBGM();
-                if (Input.GetKeyDown(KeyCode.C))
-                    DestroyBGM();
-                if (Input.GetKeyDown(KeyCode.D))
-                    PlayBGM();
-            }
-            #endregion
-            #region Public Methods
-            public void PlayBGM()
-            {
-                if (audioSource != null)
-                    return;
-                audioSource = sourceControls.PlayMusic(clip, mixerGroup);
-            }
-            public void StopBGM()
-            {
-                audioSource.Pause();
-                return;
-            }
-            public void ResumeBGM()
-            {
-                audioSource.Play();
-            }
-            public void DestroyBGM()
-            {
-                Destroy(audioSource.gameObject);
-            }
-            #endregion
-            #region Private Methods
-            #endregion
+            dimention3,
+            dimention2
+            
+        }
+    private AudioSource audioSource;
+    public AudioMixerGroup mixerGroup;
+    public AudioClip clip;
+    #endregion
+    #region Private Variables
+    SoundControls sourceControls;
+    #endregion
+    #region Lifecycle
+    private void Awake()
+    {
+        sourceControls = GameObject.Find("SoundManager").GetComponent<SoundControls>();
+    }
+    #endregion
+    #region Public Methods
+    public void PlaySFX()
+    {
+        if (audioSource != null)
+            return;
+        audioSource = sourceControls.Play2DSound(clip, mixerGroup,false);
+    }
+
+    #endregion
+    #region Private Methods
+    #endregion
         }
     }
 
