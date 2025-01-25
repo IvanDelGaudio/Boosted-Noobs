e523b5477343f8f5ed524382c29b7cd17c4e32ed
Mattia <mattia.masiero99@gmail.com>
2025-01-25 16:28:02 +0100

    Game Over screen

--- Assets/Scripts/Flow/GameManager.cs
+++ Assets/Scripts/Flow/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,7 +9,13 @@ public class GameManager : MonoBehaviour
     [Header("Data")]
     [SerializeField]
     private GameObject pausePanel;
-    
+    [SerializeField]
+    private GameObject gamOverPanel;
+    [SerializeField]
+    private GameObject subPanel;
+    [SerializeField]
+    private GameObject checkPointPanel;
+
     private bool isPaused = false;
     private SceneHandler sceneHandler;
     #endregion
@@ -19,6 +26,9 @@ public class GameManager : MonoBehaviour
         sceneHandler = GetComponent<SceneHandler>();
 
         pausePanel.SetActive(false);
+        gamOverPanel.SetActive(false);
+        subPanel.SetActive(false);
+        checkPointPanel.SetActive(false);
     }
 
     private void Update()
@@ -31,7 +41,7 @@ public class GameManager : MonoBehaviour
             }
             else
             {
-                Pause();
+                Pause(pausePanel);
             }
         }
     }
@@ -41,18 +51,42 @@ public class GameManager : MonoBehaviour
     public void Resume()
     {
         DeactivatePanel(pausePanel);
+
         Time.timeScale = 1f;
         isPaused = false;
     }
 
+
     public void GoToMenu()
     {
+        Resume();
         sceneHandler.LoadScene("Menu");
     }
+
+    public void GameOver()
+    {
+        Pause(gamOverPanel);
+    }
+
+    public void RespawnCheckpoint()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
 
     public void ActivatePanelForSeconds(GameObject panel, float duration)
     {
         StartCoroutine(ActivateThenDeactivate(panel, duration));
+    }
+    #endregion
+
+    #region Private Variables
+    private void Pause(GameObject panel)
+    {
+        ActivatePanel(panel);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     private IEnumerator ActivateThenDeactivate(GameObject panel, float duration)
@@ -62,23 +96,14 @@ public class GameManager : MonoBehaviour
         DeactivatePanel(panel);
     }
 
-    public void ActivatePanel(GameObject panel)
+    private void ActivatePanel(GameObject panel)
     {
         panel.SetActive(true);
     }
 
-    public void DeactivatePanel(GameObject panel)
+    private void DeactivatePanel(GameObject panel)
     {
         panel.SetActive(false);
-    }
-    #endregion
-
-    #region Private Variables
-    private void Pause()
-    {
-        ActivatePanel(pausePanel);
-        Time.timeScale = 0f;
-        isPaused = true;
     }
     #endregion
 }
