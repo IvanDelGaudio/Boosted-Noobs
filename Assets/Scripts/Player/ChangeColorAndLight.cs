fb5b8607253f1b03413e4101d41cdde14550b641
IvanDelGaudio <ivandelgaudio0@gmail.com>
2025-01-25 20:32:24 +0100

    init insert collider

---
+++ Assets/Scripts/Player/ChangeColorAndLight.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorAndLight : MonoBehaviour
+{
+    #region Public Variables
+    public Light light;
+    public Material material;
+    public Renderer button;
+    #endregion
+    #region Private Variables
+    #endregion
+    #region Lifecycle
+    #endregion
+    #region Public Methods
+    public void ColorAndLight()
+    {
+        
+    }
+    #endregion
+    #region Private Methods
+    #endregion
+}
