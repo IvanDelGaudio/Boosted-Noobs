a6b659a73b72ad6c6d392fd4b1e3a8f8259478bf
Francesco Del Vecchio <francescodvecchio13@gmail.com>
2026-08-18 15:48:01 +0200

    repush

--- Assets/Scripts/HUD/Subtitles/SubPhrasesList.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PhraseData", menuName = "Subtitle/Phrases list", order = 1)]
-public class SubPhrasesList : ScriptableObject
-{
-    [TextArea]
-    public string[] phrases;
-}
